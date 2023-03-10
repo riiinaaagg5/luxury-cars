77b184500fa3825441092a6b799f3979c93d29fb
riiinaaagg5 <arinkavakulenko1108@gmail.com>
2023-03-10 19:52:32 +0100

    добавляем изменения в анимацию

---
+++ script.js
@@ -0,0 +1,46 @@
+ScrollReveal().reveal(".title", {
+  reset: true,
+  distance: "350px",
+  origin: "left",
+  duration: 1000,
+});
+
+ScrollReveal().reveal(".first-screen__title", {
+    reset: true,
+    distance: "350px",
+    origin: "right",
+    duration: 2000,
+  });
+
+  ScrollReveal().reveal(".first-screen__text", {
+    delay: 375,
+    duration: 500,
+    reset: true
+  });
+
+  ScrollReveal().reveal(".first-screen__text", {
+    delay: 375,
+    duration: 2000,
+    origin: "left",
+    reset: false,
+    distance: "500px",
+  });
+
+  ScrollReveal().reveal(".main-header__item", {
+    duration: 1300,
+    origin: "top",
+    reset: false,
+    distance: "500px",
+  });
+
+  ScrollReveal().reveal(".main-header__img", {
+    duration: 2000,
+    origin: "right",
+    reset: false,
+    distance: "2000px",
+  });
+
+
+
+
+
